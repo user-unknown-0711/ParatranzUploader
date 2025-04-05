eff98b0bcf896a9414d4294d9b9851bf84623412
sora <myunligh+github@gmail.com>
2025-04-05 19:46:27 +0800

    fix script and add release action

--- scripts/list_all_file_to_change.sh
+++ scripts/list_all_file_to_change.sh
@@ -2,9 +2,13 @@
 
 cd "$(dirname "$0")" && cd ..
 
+mkdir -p dump
+
 cd Assets
 
 prefix="${1:-M}"
 
 git ls-files | grep kr/ | sed "s/^/${prefix}\t/" > ../dump/kr_files.txt
+git ls-files | grep en/ | sed "s/^/${prefix}\t/" > ../dump/en_files.txt
+git ls-files | grep jp/ | sed "s/^/${prefix}\t/" > ../dump/jp_files.txt
 git log -1 --pretty=format:"%h" > ../dump/assets_last_commit.txt
