1e2797ac0715f3ed6b77b4a1ac950c8c9ce69401
sora <myunligh+github@gmail.com>
2025-04-10 23:33:17 +0800

    fix download font error

--- scripts/copy_font.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-cd "$(dirname "$0")" && cd ..
-
-mkdir -p export/LimbusCompany_Data/Lang/TW/Font
-
-cp fonts/* export/LimbusCompany_Data/Lang/TW/Font/
--- scripts/download_font.sh
+++ scripts/download_font.sh
@@ -5,5 +5,5 @@ cd "$(dirname "$0")" && cd ..
 mkdir -p export/LimbusCompany_Data/Lang/TW/Font
 
 curl -L -o export/LimbusCompany_Data/Lang/TW/Font/SourceHanSansHC-Bold.otf \
-https://github.com/adobe-fonts/source-han-sans/raw/refs/heads/release/OTF/TraditionalChinese/SourceHanSansHC-Bold.otf
+https://github.com/adobe-fonts/source-han-sans/raw/refs/heads/release/OTF/TraditionalChineseHK/SourceHanSansHC-Bold.otf
 
