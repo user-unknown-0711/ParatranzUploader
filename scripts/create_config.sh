3a58d510964a0d62f73acc3be3ec148339a29a33
sora <myunligh+github@gmail.com>
2025-05-01 19:56:42 +0800

    update font settings

--- scripts/create_config.sh
+++ scripts/create_config.sh
@@ -4,4 +4,4 @@ cd "$(dirname "$0")" && cd ..
 
 mkdir -p export/LimbusCompany_Data/Lang
 
-echo "{\"lang\": \"TW\"}" > export/LimbusCompany_Data/Lang/config.json
+echo '{"lang": "TW","samplingPointSize": 200,"padding": 10}' > export/LimbusCompany_Data/Lang/config.json
