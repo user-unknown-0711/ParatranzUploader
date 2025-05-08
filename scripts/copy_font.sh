b42df9a646bcb8c59b252bcb8319a0d3984cb54f
sora <myunligh+github@gmail.com>
2025-05-08 18:21:39 +0800

    update release step

--- scripts/copy_font.sh
+++ scripts/copy_font.sh
@@ -6,4 +6,4 @@ mkdir -p export/LimbusCompany_Data/Lang/TW/Font/Context
 mkdir -p export/LimbusCompany_Data/Lang/TW/Font/Title
 
 cp fonts/SarasaGothicTC-Bold.ttf export/LimbusCompany_Data/Lang/TW/Font/Context/Context.ttf
-# cp fonts/SarasaGothicTC-Bold.ttf export/LimbusCompany_Data/Lang/TW/Font/Title/Title.ttf
+cp fonts/SarasaGothicTC-Bold.ttf export/LimbusCompany_Data/Lang/TW/Font/Title/Title.ttf
